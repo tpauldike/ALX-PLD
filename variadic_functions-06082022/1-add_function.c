3ef16290a631a254708c2c4ce98d38673aba418b
tpauldike <topman4loveworld@gmail.com>
2022-08-06 11:27:19 +0100

    Writing a function that adds two numbers

---
+++ variadic_functions-06082022/1-add_function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * add_2_num - A function that adds any two integers
+ * @x: the variable that holds the first number
+ * @y: the variable that holds the second number
+ * Return: the sum of x and y
+ */
+int add_2_num(int x, int y)
+{
+	return (x + y);
+}
+
+
+
+/**
+ * main - Entry point of the program
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 2, b = 7, sum = add_2_num(a, b);
+	printf("%i + %i = %i\n", a, b, sum);
+
+	return(0);
+}
