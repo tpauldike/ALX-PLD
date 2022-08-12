a1c5f6dd9076e7ba68839056d2d3754738346321
Abiodun-Shittu <jerrywizklay1@gmail.com>
2022-08-12 17:17:57 +0100

    arrays and strings updated

--- Arrays_String/array.c
+++ Arrays_String/array.c
@@ -2,17 +2,26 @@
 
 int main()
 {
+	//storing a value to a variable
+	int num = 7;
 
-    int num = 7;
-    int num2[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	//storing multiple values in a single variable with the help of an array;
+	int num2[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    //zero-based index --> position starting from zero
+	/**
+	 * zero-based index --> position starting from zero
+	 * first position of the array have index zero
+	 */
 
-    printf("the first element of the array is %d\n", num2[0]);
+	printf("the first element of the array is %d\n", num2[0]);
 
-    printf("the last element of the array is %d\n", num2[8]);
+	printf("the last element of the array is %d\n", num2[8]);
 
-    printf("any element of the arrray is %d\n", num2[12]);
+	/**
+	 * this will print out a random value for index 12
+	 * because there are just 9 elements in the array
+	 */
+	printf("any element of the arrray is %d\n", num2[12]);
 
-    return (0);
+	return (0);
 }
--- Arrays_String/string.c
+++ Arrays_String/string.c
@@ -2,30 +2,35 @@
 
 void main()
 {
-    char greeting[] = "Hello, world";
-    /**
-     * greeting[0] = H i = 0
-     * greeting[1] = e
-     * greeting[2] = l
-     * greeting[3] = l    greeting[i] != '\0'--. greeting[i]
-     * greeting[4] = o
-     * greeting[5] = '\0'
-    */  
-    printf("the third letter of the string is %c\n", greeting[2]);
+	char greeting[] = "Hello, world";
+	/**
+	 * greeting[0] = H i = 0
+	 * greeting[1] = e
+	 * greeting[2] = l
+	 * greeting[3] = l    greeting[i] != '\0' --> greeting[i]
+	 * greeting[4] = o
+	 * greeting[5] = '\0'
+	 */
+	printf("the third letter of the string is %c\n", greeting[2]);
 
-    greeting[2] = 'A';
+	greeting[2] = 'A'; // string mutation
 
-    int i;
+	/** ########### For loop syntax ##########
+	 * for(initialization; condition; update)
+	 * {
+	 *  	code to be executed if condition is met;
+	 * }
+	 */
+	int i;
 
-    for (i = 0; greeting[i] != '\0'; i++)
-    {
-        printf("%c", greeting[i]);
-    }
-
-    printf("\nSuccess!!!!\n\n");
+	for (i = 0; greeting[i] != '\0'; i++)
+	{
+		printf("%c", greeting[i]);
+	}
+	printf("\nSuccess!!!!\n");
 }
 
-
+/*############# How the for loop runs #################*/
 // step1 --> i = 0
 // step2 --> greeting[i] != '\0 true if the char at that position is not the end
 // step3 --> printf("%c\n", greeting[i]);
