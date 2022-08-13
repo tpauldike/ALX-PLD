1c3f5e2d2ce1966fbacbd734c46228409b0a1a7d
tpauldike <topman4loveworld@gmail.com>
2022-08-13 17:15:08 +0000

    File update

--- variadic_functions-06082022/3-variadic_function.c
+++ variadic_functions-06082022/3-variadic_function.c
@@ -14,8 +14,8 @@ int add_up(int num, ...)
         sum = 0;
         for (index = 0; index < num; index++)
         {
-                sum += va_arg(our_list, int);
-        }
+                sum += va_arg(our_list, int); //sum = sum + (next member)
+	}
         printf("%d\n", sum);
         va_end(our_list);
 
@@ -24,10 +24,10 @@ int add_up(int num, ...)
 
 int main(void)
 {
-        add_up(5, 2, 1, 3, 2, 1);
-        add_up(3, 6, 1, 2);
-        add_up(7, 1, 1, 3, 2, 1, 1, 2);
-        
+        add_up(5, 2, 1, 3, 2, 1); //9
+        add_up(3, 6, 1, 2);       //9
+        add_up(7, 1, 1, 3, 2, 1, 1, 2); //11
+
         /**
 	 * Line number 38 was executed as follows:
          * since the first argument(3) represents the number
@@ -43,7 +43,7 @@ int main(void)
          * the problem is going to assume any 2 variables of the type
          * int to complete a total of 7 variables and then add.
          */
-        add_up(7, 1, 1, 3, 2, 3);
-    
+	add_up(7, 1, 1, 3, 2, 3);
+
         return (0);
 }
