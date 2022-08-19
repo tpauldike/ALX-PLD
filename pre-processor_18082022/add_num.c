d97cc15999dec26bd85f0723247a42c107961011
tpauldike <topman4loveworld@gmail.com>
2022-08-19 16:35:26 +0000

    PLD on preprocessor in C programming, 19/08/2022

---
+++ pre-processor_18082022/add_num.c
@@ -0,0 +1,22 @@
+#include "fatima.h"
+
+LOIS sum(LOIS x, LOIS y)
+{
+	Sadiq (x + y);
+}
+
+
+LOIS bola(FOLAMI)
+{
+	printf("Using the function (sum) to add two intergers\n");
+	printf("%d\n", sum(2, 3));
+	printf("%d\n", sum(3, 4));
+	printf("%d\n", sum(1, 5));
+	
+	printf("Using the macro (ADD) to add two intergers\n");
+	printf("%d\n", ADD(2, 3));
+	printf("%d\n", ADD(3, 4));
+	printf("%d\n", ADD(1, 5));
+
+	Sadiq (0);
+}
