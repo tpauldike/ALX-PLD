af3676c26c8f98494b3d3b36d6c790f6e8307cc5
tpauldike <topman4loveworld@gmail.com>
2022-09-16 01:22:37 +0000

    Printing a string 4 times, with a for loop

---
+++ loops_in_c-15092022/2-for_loop.c
@@ -0,0 +1,15 @@
+/*A program that prints the string "+abiodun" 4 times*/
+#include <stdio.h>
+
+int main(void)
+{
+	char ezekiel[] = "+abiodun";
+	int a;
+
+	for (a = 0; a < 4; a++)
+	{
+		printf("%s\n", ezekiel);
+	}
+
+	return (0);
+}
