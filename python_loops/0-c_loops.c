891c2ae053e4eed6eb40914a2678e02634542fb9
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-12 23:48:28 +0100

    Loops in C and Python

--- python_loops/0-c_loops.c
+++ python_loops/0-c_loops.c
@@ -8,12 +8,21 @@
 int main(void)
 {
 	char fname[20];
+	int age;
 
 	printf("Please enter your first name: ");
 	scanf("%s", fname);	 // %c is for a single character, e.g; 'a'
 	// while %s is for more than 1 character (i.e, strings). e.g; 'abc'
-	printf("Welcome %s\n", fname);
-		//Welcome ____
+	
+	printf("Enter your age: ");
+	scanf("%d", &age);
+
+	if (age < 18)
+		printf("sorry %s, you are too young and not qualified\n", fname);
+	else if (age >= 18 && age <= 40)
+		printf("Welcome %s, You have access to continue!!!\n", fname);
+	else
+		printf("Oops, You are too old to be here, %s\n", fname);
 
 	return (0);
 }
