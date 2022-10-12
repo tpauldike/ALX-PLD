61b26cfbe0772222903e3b1f93d356121d9fea2c
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-10-12 16:02:49 +0100

    ADding 10 integers of 2 sets

--- Functions/2-function.c
+++ Functions/2-function.c
@@ -8,7 +8,31 @@ int main(void)
 	printf("%i + %i = %i\n", num1, num2, addition1);
 	
 	int num3 = 7, num4 = 4, addition2 = num3 + num4;
-	printf("%i + %i = %i\n", num1, num2, addition2);
+	printf("%i + %i = %i\n", num3, num4, addition2);
+
+	int num5 = 10, num6 = 19, addition3 = num5 + num6;
+	printf("%i + %i = %i\n", num5, num6, addition3);
+
+	int num7 = 6, num8 = 6, addition4 = num7 + num8;
+	printf("%i + %i = %i\n", num7, num8, addition4);
+
+	int num9 = 56, num10 = 10, addition5 = num9 + num10;
+	printf("%i + %i = %i\n", num9, num10, addition5);
+
+	int num11 = -66, num12 = 23, addition6 = num11 + num12;
+	printf("%i + %i = %i\n", num11, num12, addition6);
+
+	int num13 = -100, num14 = -24, addition7 = num13 + num14;
+	printf("%i + %i = %i\n", num13, num14, addition7);
+
+	int num15 = 70, num16 = 40, addition8 = num15 + num16;
+	printf("%i + %i = %i\n", num15, num16, addition8);
+
+	int num17 = 52, num18 = 34, addition9 = num17 + num18;
+	printf("%i + %i = %i\n", num17, num18, addition9);
+
+	int num19 = -197, num20 = -100, addition10 = num19 + num20;
+	printf("%i + %i = %i\n", num19, num20, addition10);
 
 	return (0);
 }
