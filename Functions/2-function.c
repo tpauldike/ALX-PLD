bfdc832474603343d75ff75d5e65f07bf9d41056
tpauldike <topman4loveworld@gmail.com>
2022-10-12 14:51:46 +0000

    adding 10 sets of two integers

---
+++ Functions/2-function.c
@@ -0,0 +1,14 @@
+/* A program that adds two integers and returns the sum*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1 = 5, num2 = 3, addition1 = num1 + num2;
+	printf("%i + %i = %i\n", num1, num2, addition1);
+	
+	int num3 = 7, num4 = 4, addition2 = num3 + num4;
+	printf("%i + %i = %i\n", num1, num2, addition2);
+
+	return (0);
+}
